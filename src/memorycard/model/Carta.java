2433fa00e2c2c0e49c88b2a5e78d296c72e806c8
Corujaoprogramador <deivid9pereira@gmail.com>
2025-07-09 11:52:47 -0300

    model base: carta e jogador

--- src/memorycard/model/Carta.java
+++ src/memorycard/model/Carta.java
@@ -1,5 +1,53 @@
 package memorycard.model;
 
 public class Carta {
-    
+    private int id;
+    private String imagem;
+    private boolean virada;
+    private boolean encontrada;
+
+    public Carta(int id, String imagem) {
+        this.id = id;
+        this.imagem = imagem;
+        this.virada = false;
+        this.encontrada = false;
+    }
+
+    public void virar() {
+        this.virada = !this.virada;
+    }
+
+    public boolean comparar(Carta outra) {
+        return this.id == outra.id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public boolean isVirada() {
+        return virada;
+    }
+
+    public boolean isEncontrada() {
+        return encontrada;
+    }
+
+    public void setEncontrada(boolean encontrada) {
+        this.encontrada = encontrada;
+    }
+
+    @Override
+    public String toString() {
+        return "Carta{" +
+                "id=" + id +
+                ", virada=" + virada +
+                ", encontrada=" + encontrada +
+                '}';
+    }
 }
+
--- src/memorycard/model/Jogador.java
+++ src/memorycard/model/Jogador.java
@@ -1,5 +1,32 @@
 package memorycard.model;
 
 public class Jogador {
-    
+    private String nome;
+    private int pontuacao;
+
+    public Jogador(String nome) {
+        this.nome = nome;
+        this.pontuacao = 0;
+    }
+
+    public void incrementarPontos() {
+        pontuacao++;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (" + pontuacao + " pontos)";
+    }
 }
