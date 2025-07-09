b6ca16ef8100f5807c7b1914353edf4e831d76a2
Corujaoprogramador <deivid9pereira@gmail.com>
2025-07-09 12:29:23 -0300

    model: partida e tabuleiro

--- src/memorycard/model/Partida.java
+++ src/memorycard/model/Partida.java
@@ -1,5 +1,92 @@
 package memorycard.model;
 
 public class Partida {
-    
+    private Jogador jogador1;
+    private Jogador jogador2;
+    private Tabuleiro tabuleiro;
+
+    private Jogador jogadorAtual;
+    private int primeiraCarta = -1;
+    private int segundaCarta = -1;
+
+    public Partida(Jogador j1, Jogador j2, int qtdPares) {
+        this.jogador1 = j1;
+        this.jogador2 = j2;
+        this.tabuleiro = new Tabuleiro(qtdPares);
+        this.jogadorAtual = j1;
+    }
+
+    public boolean selecionarCarta(int indice) {
+        Carta carta = tabuleiro.getCarta(indice);
+        if (carta == null || carta.isVirada() || carta.isEncontrada()) {
+            return false;
+        }
+
+        carta.virar();
+
+        if (primeiraCarta == -1) {
+            primeiraCarta = indice;
+        } else if (segundaCarta == -1) {
+            segundaCarta = indice;
+        }
+
+        return true;
+    }
+
+    public boolean podeVerificarPar() {
+        return primeiraCarta != -1 && segundaCarta != -1;
+    }
+
+    public boolean verificarParSelecionado() {
+        if (!podeVerificarPar()) return false;
+
+        boolean par = tabuleiro.verificarPar(primeiraCarta, segundaCarta);
+        if (par) {
+            jogadorAtual.incrementarPontos();
+        } else {
+            tabuleiro.getCarta(primeiraCarta).virar();
+            tabuleiro.getCarta(segundaCarta).virar();
+            alternarTurno();
+        }
+
+        primeiraCarta = -1;
+        segundaCarta = -1;
+
+        return par;
+    }
+
+    private void alternarTurno() {
+        jogadorAtual = (jogadorAtual == jogador1) ? jogador2 : jogador1;
+    }
+
+    public boolean isJogoFinalizado() {
+        for (Carta carta : tabuleiro.getCartas()) {
+            if (!carta.isEncontrada()) return false;
+        }
+        return true;
+    }
+
+    public Jogador getVencedor() {
+        if (!isJogoFinalizado()) return null;
+
+        if (jogador1.getPontuacao() > jogador2.getPontuacao()) return jogador1;
+        if (jogador2.getPontuacao() > jogador1.getPontuacao()) return jogador2;
+        return null;
+    }
+
+    public Jogador getJogadorAtual() {
+        return jogadorAtual;
+    }
+
+    public Jogador getJogador1() {
+        return jogador1;
+    }
+
+    public Jogador getJogador2() {
+        return jogador2;
+    }
+
+    public Tabuleiro getTabuleiro() {
+        return tabuleiro;
+    }
 }
--- src/memorycard/model/Tabuleiro.java
+++ src/memorycard/model/Tabuleiro.java
@@ -1,5 +1,54 @@
 package memorycard.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Tabuleiro {
-    
+    private Carta[] cartas;
+
+    public Tabuleiro(int qtdPares) {
+        inicializarCartas(qtdPares);
+    }
+
+    private void inicializarCartas(int qtdPares) {
+        List<Carta> baralhoTemp = new ArrayList<>();
+
+        for (int i = 0; i < qtdPares; i++) {
+            baralhoTemp.add(new Carta(i, "img" + i + ".png"));
+            baralhoTemp.add(new Carta(i, "img" + i + ".png"));
+        }
+
+        Collections.shuffle(baralhoTemp);
+
+        cartas = baralhoTemp.toArray(new Carta[0]);
+    }
+
+    public Carta getCarta(int indice) {
+        if (indice >= 0 && indice < cartas.length) {
+            return cartas[indice];
+        }
+        return null;
+    }
+
+    public boolean verificarPar(int i1, int i2) {
+        Carta c1 = getCarta(i1);
+        Carta c2 = getCarta(i2);
+
+        if (c1 != null && c2 != null && c1.comparar(c2)) {
+            c1.setEncontrada(true);
+            c2.setEncontrada(true);
+            return true;
+        }
+
+        return false;
+    }
+
+    public int getTamanho() {
+        return cartas.length;
+    }
+
+    public Carta[] getCartas() {
+        return cartas;
+    }
 }
